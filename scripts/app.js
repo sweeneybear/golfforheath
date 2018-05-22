ce56ee8c24061ee94fd7077a541ae3fba2f8ecdf
Rachel Sweeney <rachelgsweeney@gmail.com>
2018-05-22 09:39:44 -0600

    update scripts for click

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
   var $menu = $("#sidebar-wrapper");
 
-  $(document).on("click", ".js-menu-open", function(evt) {
-    $menu.addClass("open");
-    return evt.target.tagName === "A";
-  });
-  $(document).on("click", ".js-menu-close", function(evt) {
-    $menu.removeClass("open");
-    return evt.target.tagName === "A";
-  });
+  $(document)
+    .on("click", ".js-menu-open", function(evt) {
+      $menu.addClass("open");
+
+      return evt.target.tagName === "A";
+    })
+    .on("click", ".js-menu-close", function(evt) {
+      $menu.removeClass("open");
+
+      return evt.target.tagName === "A";
+    });
 });
